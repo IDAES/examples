4246adb0c7f0eaa98ca0884933a3e78099fa6de1
Keith Beattie <KSBeattie@lbl.gov>
2024-04-12 17:21:47 -0700

    `relase` should be `release`

--- idaes_examples/notebooks/docs/surrogates/sco2/omlt/properties.py
+++ idaes_examples/notebooks/docs/surrogates/sco2/omlt/properties.py
@@ -140,7 +140,7 @@ class _StateBlock(StateBlock):
                                  initialization.
                         - False - state variables are unfixed after
                                  initialization by calling the
-                                 relase_state method
+                                 release_state method
 
         Returns:
             If hold_states is True, returns a dict containing flags for
@@ -200,7 +200,7 @@ class _StateBlock(StateBlock):
 
     def release_state(blk, flags, outlvl=0):
         '''
-        Method to relase state variables fixed during initialisation.
+        Method to release state variables fixed during initialisation.
 
         Keyword Arguments:
             flags : dict containing information of which state variables
