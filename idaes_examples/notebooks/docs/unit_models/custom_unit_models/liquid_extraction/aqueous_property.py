a5dee4510f46395a766a4a47f8a751267bcfd18b
JavalVyas2000 <javalvyas@gmail.com>
2024-05-06 19:23:53 -0400

    adding requested changes

--- idaes_examples/notebooks/docs/unit_models/custom_unit_models/liquid_extraction/aqueous_property.py
+++ idaes_examples/notebooks/docs/unit_models/custom_unit_models/liquid_extraction/aqueous_property.py
@@ -51,12 +51,12 @@ _log = logging.getLogger(__name__)
 
 
 @declare_process_block_class("AqPhase")
-class PhysicalParameterData(PhysicalParameterBlock):
+class AqPhaseData(PhysicalParameterBlock):
     """
     Property Parameter Block Class
 
     Contains parameters and indexing sets associated with properties for
-    liquid Phase
+    aqueous Phase
 
     """
 
@@ -76,8 +76,7 @@ class PhysicalParameterData(PhysicalParameterBlock):
         self.KNO3 = Solute()
         self.CaSO4 = Solute()
         self.H2O = Solvent()
-
-        self.solutes = Set(initialize=["NaCl", "KNO3", "CaSO4"])
+        
         # Heat capacity of solvent
         self.cp_mass = Param(
             mutable=True,
@@ -119,92 +118,8 @@ class _StateBlock(StateBlock):
     whole, rather than individual elements of indexed Property Blocks.
     """
 
-    def initialize(
-        self,
-        state_args=None,
-        state_vars_fixed=False,
-        hold_state=False,
-        outlvl=idaeslog.NOTSET,
-        solver=None,
-        optarg=None,
-    ):
-        """
-        Initialization routine for property package.
-        Keyword Arguments:
-            state_args : Dictionary with initial guesses for the state vars
-                         chosen. Note that if this method is triggered
-                         through the control volume, and if initial guesses
-                         were not provided at the unit model level, the
-                         control volume passes the inlet values as initial
-                         guess.The keys for the state_args dictionary are:
-            flow_mol_comp : value at which to initialize component flows (default=None)
-            pressure : value at which to initialize pressure (default=None)
-            temperature : value at which to initialize temperature (default=None)
-            outlvl : sets output level of initialization routine
-            state_vars_fixed: Flag to denote if state vars have already been fixed.
-                              True - states have already been fixed and
-                              initialization does not need to worry
-                              about fixing and unfixing variables.
-                              False - states have not been fixed. The state
-                              block will deal with fixing/unfixing.
-            optarg : solver options dictionary object (default=None, use
-                     default solver options)
-            solver : str indicating which solver to use during
-                     initialization (default = None, use default solver)
-            hold_state : flag indicating whether the initialization routine
-                         should unfix any state variables fixed during
-                         initialization (default=False).
-                         True - states variables are not unfixed, and
-                         a dict of returned containing flags for
-                         which states were fixed during initialization.
-                         False - state variables are unfixed after
-                         initialization by calling the
-                         release_state method
-        Returns:
-            If hold_states is True, returns a dict containing flags for
-            which states were fixed during initialization.
-        """
-        init_log = idaeslog.getInitLogger(self.name, outlvl, tag="properties")
-
-        if state_vars_fixed is False:
-            # Fix state variables if not already fixed
-            flags = fix_state_vars(self, state_args)
-
-        else:
-            # Check when the state vars are fixed already result in dof 0
-            for k in self.keys():
-                if degrees_of_freedom(self[k]) != 0:
-                    raise Exception(
-                        "State vars fixed but degrees of freedom "
-                        "for state block is not zero during "
-                        "initialization."
-                    )
-
-        if state_vars_fixed is False:
-            if hold_state is True:
-                return flags
-            else:
-                self.release_state(flags)
-
-        init_log.info("Initialization Complete.")
-
-    def release_state(self, flags, outlvl=idaeslog.NOTSET):
-        """
-        Method to release state variables fixed during initialization.
-        Keyword Arguments:
-            flags : dict containing information of which state variables
-                    were fixed during initialization, and should now be
-                    unfixed. This dict is returned by initialize if
-                    hold_state=True.
-            outlvl : sets output level of logging
-        """
-        init_log = idaeslog.getInitLogger(self.name, outlvl, tag="properties")
-
-        if flags is None:
-            return
-        # Unfix state variables
-        revert_state_vars(self, flags)
-        init_log.info("State Released.")
+    def fix_initialization_states(self):
+        fix_state_vars(self)
 
 
 @declare_process_block_class("AqPhaseStateBlock", block_class=_StateBlock)
@@ -229,7 +144,7 @@ class AqPhaseStateBlockData(StateBlockData):
         )
 
         self.conc_mass_comp = Var(
-            self.params.solutes,
+            self.params.solute_set,
             domain=NonNegativeReals,
             initialize={"NaCl": 0.15, "KNO3": 0.2, "CaSO4": 0.1},
             doc="Component mass concentrations",
--- idaes_examples/notebooks/docs/unit_models/custom_unit_models/liquid_extraction/organic_property.py
+++ idaes_examples/notebooks/docs/unit_models/custom_unit_models/liquid_extraction/organic_property.py
@@ -80,7 +80,6 @@ class PhysicalParameterData(PhysicalParameterBlock):
             Solvent()
         )  # Solvent used here is ethylene dibromide (Organic Polar)
 
-        self.solutes = Set(initialize=["NaCl", "KNO3", "CaSO4"])
         # Heat capacity of solvent
         self.cp_mass = Param(
             mutable=True,
@@ -103,7 +102,7 @@ class PhysicalParameterData(PhysicalParameterBlock):
             units=units.K,
         )
         self.diffusion_factor = Param(
-            self.solutes,
+            self.solute_set,
             initialize={"NaCl": 2.15, "KNO3": 3, "CaSO4": 1.5},
             within=PositiveReals,
             mutable=True,
@@ -128,93 +127,9 @@ class _StateBlock(StateBlock):
     whole, rather than individual elements of indexed Property Blocks.
     """
 
-    def initialize(
-        self,
-        state_args=None,
-        state_vars_fixed=False,
-        hold_state=False,
-        outlvl=idaeslog.NOTSET,
-        solver=None,
-        optarg=None,
-    ):
-        """
-        Initialization routine for property package.
-        Keyword Arguments:
-            state_args : Dictionary with initial guesses for the state vars
-                         chosen. Note that if this method is triggered
-                         through the control volume, and if initial guesses
-                         were not provided at the unit model level, the
-                         control volume passes the inlet values as initial
-                         guess.The keys for the state_args dictionary are:
-            flow_mol_comp : value at which to initialize component flows (default=None)
-            pressure : value at which to initialize pressure (default=None)
-            temperature : value at which to initialize temperature (default=None)
-            outlvl : sets output level of initialization routine
-            state_vars_fixed: Flag to denote if state vars have already been fixed.
-                              True - states have already been fixed and
-                              initialization does not need to worry
-                              about fixing and unfixing variables.
-                              False - states have not been fixed. The state
-                              block will deal with fixing/unfixing.
-            optarg : solver options dictionary object (default=None, use
-                     default solver options)
-            solver : str indicating which solver to use during
-                     initialization (default = None, use default solver)
-            hold_state : flag indicating whether the initialization routine
-                         should unfix any state variables fixed during
-                         initialization (default=False).
-                         True - states variables are not unfixed, and
-                         a dict of returned containing flags for
-                         which states were fixed during initialization.
-                         False - state variables are unfixed after
-                         initialization by calling the
-                         release_state method
-        Returns:
-            If hold_states is True, returns a dict containing flags for
-            which states were fixed during initialization.
-        """
-        init_log = idaeslog.getInitLogger(self.name, outlvl, tag="properties")
-
-        if state_vars_fixed is False:
-            # Fix state variables if not already fixed
-            flags = fix_state_vars(self, state_args)
-
-        else:
-            # Check when the state vars are fixed already result in dof 0
-            for k in self.keys():
-                if degrees_of_freedom(self[k]) != 0:
-                    raise Exception(
-                        "State vars fixed but degrees of freedom "
-                        "for state block is not zero during "
-                        "initialization."
-                    )
-
-        if state_vars_fixed is False:
-            if hold_state is True:
-                return flags
-            else:
-                self.release_state(flags)
-
-        init_log.info("Initialization Complete.")
-
-    def release_state(self, flags, outlvl=idaeslog.NOTSET):
-        """
-        Method to release state variables fixed during initialization.
-        Keyword Arguments:
-            flags : dict containing information of which state variables
-                    were fixed during initialization, and should now be
-                    unfixed. This dict is returned by initialize if
-                    hold_state=True.
-            outlvl : sets output level of logging
-        """
-        init_log = idaeslog.getInitLogger(self.name, outlvl, tag="properties")
-
-        if flags is None:
-            return
-        # Unfix state variables
-        revert_state_vars(self, flags)
-        init_log.info("State Released.")
-
+    def fix_initialization_states(self):
+        fix_state_vars(self)
+    
 
 @declare_process_block_class("OrgPhaseStateBlock", block_class=_StateBlock)
 class LiqPhaseStateBlockData(StateBlockData):
@@ -237,7 +152,7 @@ class LiqPhaseStateBlockData(StateBlockData):
             units=units.L / units.hour,
         )
         self.conc_mass_comp = Var(
-            self.params.solutes,
+            self.params.solute_set,
             domain=NonNegativeReals,
             initialize=1,
             doc="Component mass concentrations",
