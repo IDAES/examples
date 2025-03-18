28339f0ce7f0eecab35de139d7934b5d9fa477e3
Dan Gunter <dkgunter@lbl.gov>
2025-03-17 17:25:45 -0700

    modified file headers

--- idaes_examples/notebooks/docs/surrogates/pysmo/rbf_griewank_function.py
+++ idaes_examples/notebooks/docs/surrogates/pysmo/rbf_griewank_function.py
@@ -3,12 +3,13 @@
 # Framework (IDAES IP) was produced under the DOE Institute for the
 # Design of Advanced Energy Systems (IDAES).
 #
-# Copyright (c) 2018-2023 by the software owners: The Regents of the
+# Copyright (c) 2018-2025 by the software owners: The Regents of the
 # University of California, through Lawrence Berkeley National Laboratory,
 # National Technology & Engineering Solutions of Sandia, LLC, Carnegie Mellon
 # University, West Virginia University Research Corporation, et al.
 # All rights reserved.  Please see the files COPYRIGHT.md and LICENSE.md
 # for full copyright and license information.
+#
 #################################################################################
 """
 2d-Griewank function, see Griewank (1981) paper
